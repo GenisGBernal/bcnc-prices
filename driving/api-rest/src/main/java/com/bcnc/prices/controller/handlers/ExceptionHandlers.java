421c76be2a2ac73580f0b924290175c14d62062d
GenisGBernal <genisgutierrezbernal@gmail.com>
2025-09-22 15:17:46 +0200

    fix: date input param returns 400 if invalid

--- driving/api-rest/src/main/java/com/bcnc/prices/controller/handlers/ExceptionHandlers.java
+++ driving/api-rest/src/main/java/com/bcnc/prices/controller/handlers/ExceptionHandlers.java
@@ -6,6 +6,7 @@ package com.bcnc.prices.controller.handlers;
 
 import com.bcnc.prices.application.exceptions.BadRequestException;
 import com.bcnc.prices.application.exceptions.NotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +19,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ExceptionHandlers extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(value = NotFoundException.class)
-  public ResponseEntity<Object> handleConflictException(NotFoundException e) {
+  public ResponseEntity<Object> handeException(NotFoundException ex) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
   }
 
   @ExceptionHandler(value = BadRequestException.class)
-  public ResponseEntity<Object> handleConflictException(BadRequestException e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+  public ResponseEntity<Object> handeException(BadRequestException ex) {
+    return getBadRequestResponse(ex.getMessage());
+  }
+
+  @ExceptionHandler(value = ConstraintViolationException.class)
+  public ResponseEntity<Object> handeException(ConstraintViolationException ex) {
+    return getBadRequestResponse(ex.getMessage());
+  }
+
+  private static ResponseEntity<Object> getBadRequestResponse(String ex) {
+    return ResponseEntity
+      .status(HttpStatus.BAD_REQUEST)
+      .body(ex);
   }
 }
