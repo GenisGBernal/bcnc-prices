23d43b2713f390317e65513e5e6161691265a97d
GenisGBernal <genisgutierrezbernal@gmail.com>
2025-09-22 14:52:51 +0200

    docs: postman collection and service banner

--- application/src/main/java/com/bcnc/prices/application/services/PriceService.java
+++ application/src/main/java/com/bcnc/prices/application/services/PriceService.java
@@ -14,6 +14,8 @@ public class PriceService {
 
     private final PriceRepositoryPort repositoryPort;
 
+    // If maximum performance was needed, an in-memory or shared cache could be used here.
+    // But as the query params include a date, hits would probably be low.
     public Optional<ActivePrice> findActivePrice(
         LocalDateTime date,
         Long productId,
